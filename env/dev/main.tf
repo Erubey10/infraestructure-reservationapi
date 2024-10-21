deba62d82408a110a9027c26a43bbdf3a28cf287
Emilio <emiliog@vauxoo.com>
2024-10-20 21:26:47 -0600

    Se agregan archivos de dev

---
+++ env/dev/main.tf
@@ -0,0 +1,24 @@
+module "dev_vm" {
+  source                     = "../../modules/vm"
+  resource_group             = "IN-RG-gallarditas-Dev"
+  security_group_name        = "IN-SG-gallarditas-Dev"
+  virtual_network_name       = "IN-VNET-gallarditas-Dev"
+  subnet_name                = "IN-SUBNET-gallarditas-Dev"
+  ip_name                    = "IN-IP-gallarditas-Dev"
+  location                   = "eastus2"
+  admin_username             = "adminuser"
+  nic_name                   = "IN-NIC-gallarditas-Dev"
+  vm_name                    = "IN-Server-gallarditas-Dev"
+  DOMAIN                     = var.DOMAIN
+  MAIL_SECRET_KEY            = var.MAIL_SECRET_KEY
+  MAIL_SERVICE               = var.MAIL_SERVICE
+  MAPBOX_ACCESS_TOKEN        = var.MAPBOX_ACCESS_TOKEN
+  MONGO_INITDB_ROOT_USERNAME = var.MONGO_INITDB_ROOT_USERNAME
+  MONGO_INITDB_ROOT_PASSWORD = var.MONGO_INITDB_ROOT_PASSWORD
+  MONGO_DB                   = var.MONGO_DB
+  MAIL_USER                  = var.MAIL_USER
+  PORT                       = var.PORT
+  MONGO_URL                  = var.MONGO_URL
+}
+
+// TEST MONO API 3
---
+++ env/dev/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "NetworkWatcherRG"
+    storage_account_name = "711gallarditas"
+    container_name = "711-74576-tfstate"
+    key = "terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
