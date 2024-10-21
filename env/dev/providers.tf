63b6ecf4b1c9d59e74c66e96ed6e29a30a3f79d7
CristianRC03 <crizcamach2003@gmail.com>
2024-10-20 21:55:20 -0600

    Workflows con github actions

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "NetworkWatcherRG"
-    storage_account_name = "711gallarditas"
-    container_name = "711-74576-tfstate"
-    key = "terraform.tfstate"
+    resource_group_name  = "NetworkWatcherRG"
+    storage_account_name = "711cristian"
+    container_name       = "711-74576-tfstate"
+    key                  = "terraform.tfstate"
   }
 
   required_providers {
