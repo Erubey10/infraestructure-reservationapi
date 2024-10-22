edd5bb788fa93ad0417ff46571631852b4f0eaca
CristianRC03 <crizcamach2003@gmail.com>
2024-10-21 19:38:39 -0600

    prueba

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -57,6 +57,17 @@ resource "azurerm_network_security_group" "IN_SG" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "hola"
+    priority                   = 104
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "1433"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
 
 
